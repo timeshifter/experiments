48ddc3e2d21b022f6267e1a9cc01c3ee4b311b3a
David-PC\David <chrono34@hotmail.com>
2017-09-04 15:35:43 -0400

    added ts buy log, job buying logic

--- trimpshifter.js
+++ trimpshifter.js
@@ -2,7 +2,7 @@ var _ts_enabled = true,
     _ts_gameLoopId,
     _ts_gameLoopInterval = 1000,
     _ts_i = 0,
-    _ts_version = '1.0.3'
+    _ts_version = '1.0.4'
     ;
 
 
@@ -20,20 +20,22 @@ function MainLoop() {
         return;
     }
 
+    var result = false;
+    numTab(1);
     //game.resources.food.owned
     //game.resources.food.max
 
 
     if ((game.resources.food.owned / game.resources.food.max) > 0.75) {
-        buyBuilding('Barn');
+        _ts_BuyBuilding('Barn');
     }
 
     if ((game.resources.wood.owned / game.resources.wood.max) > 0.75) {
-        buyBuilding('Shed');
+        _ts_BuyBuilding('Shed');
     }
 
     if ((game.resources.metal.owned / game.resources.metal.max) > 0.75) {
-        buyBuilding('Forge');
+        _ts_BuyBuilding('Forge');
     }
 
     var priorityUpgrades = ['Anger', 'Battle', 'Bloodlust', 'Bounty', 'Coordination', 'Efficiency', 'Egg', 'Explorers', 'Gymystic', 'Miners', 'Potency', 'Scientists', 'Speedfarming', 'Speedlumber', 'Speedminer', 'Speedscience', 'TrainTacular', 'Trainers', 'UberHotel', 'UberHouse', 'UberHut', 'UberMansion', 'UberResort'],
@@ -43,12 +45,10 @@ function MainLoop() {
     for (var i = 0; i < priorityUpgrades.length; i++) {
         if (game.upgrades[priorityUpgrades[i]].locked == 0) {
 
-            var result = buyUpgrade(priorityUpgrades[i]);
+            result = _ts_BuyUpgrade(priorityUpgrades[i]);
 
-            if (result) {
-                console.log('Upgraded ' + priorityUpgrades[i]);
-            }
-            else{
+            if (!result) {
+
                 hasPriority = true;
             }
 
@@ -56,7 +56,68 @@ function MainLoop() {
     }
 
 
+    if (game.workspaces > 0) {
+
+        if (game.jobs.Trainer.locked == 0) {
+            result = true;
+            while (result) {
+                result = _ts_BuyJob('Trainer');
+            }
+        }
+
+
+        if (game.jobs.Explorer.locked == 0) {
+            result = true;
+            while (result && game.jobs.Explorer.owned < 50) {
+                result = _ts_BuyJob('Explorer');
+            }
+        }
+        
+        var sciRatio = (game.jobs.Farmer.owned + game.jobs.Lumberjack.owned + game.jobs.Miner.owned) / game.jobs.Scientist.owned;
+
+        if (sciRatio > 25) {
+            buyJob('Scientist');
+            
+        }
+
+        if (game.jobs.Miner.owned < game.jobs.Lumberjack.owned)
+            _ts_BuyJob('Miner');
+        else if (game.jobs.Lumberjack < game.jobs.Farmer.owned)
+            _ts_BuyJob('Lumberjack');
+        else
+            _ts_BuyJob('Farmer');
+
+    }
+
+
+}
 
 
 
+function _ts_BuyJob(what) {
+    var result = buyJob(what);
+    if (result)
+        console.log('TrimpShifter - buying job ' + what);
+    return result;
+}
+
+function _ts_BuyBuilding(what) {
+    var result = buyBuilding(what);
+    if (result)
+        console.log('TrimpShifter - buying building ' + what);
+    return result;
+}
+
+function _ts_BuyUpgrade(what) {
+    var result = buyUpgrade(what);
+    if (result)
+        console.log('TrimpShifter - buying upgrade ' + what);
+    return result;
+}
+
+function _ts_BuyEquipment(what) {
+    var result = buyEquipment(what);
+    if (result)
+        console.log('TrimpShifter - buying equipment ' + what);
+    return result;
 }
