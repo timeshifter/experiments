bf3499e2ee128f2f800a7970f32584033b842d05
timeshifter <chrono34@hotmail.com>
2020-09-02 18:15:59 -0500

    finished data entry, improved UI

--- ipm_data.js
+++ ipm_data.js
@@ -71,6 +71,82 @@ var IPM_DATA = [
         reqs: []
     },
 
+    {
+        id: 'iridium',
+        name: 'Iridium',
+        type: 'ore',
+        reqs: []
+    },
+
+    {
+        id: 'paladium',
+        name: 'Paladium',
+        type: 'ore',
+        reqs: []
+    },
+
+    {
+        id: 'osmium',
+        name: 'Osmium',
+        type: 'ore',
+        reqs: []
+    },
+
+    {
+        id: 'rhodium',
+        name: 'Rhodium',
+        type: 'ore',
+        reqs: []
+    },
+
+    {
+        id: 'inerton',
+        name: 'Inerton',
+        type: 'ore',
+        reqs: []
+    },
+
+    {
+        id: 'quadium',
+        name: 'Quadium',
+        type: 'ore',
+        reqs: []
+    },
+
+    {
+        id: 'scrith',
+        name: 'Scrith',
+        type: 'ore',
+        reqs: []
+    },
+
+    {
+        id: 'uru',
+        name: 'Uru',
+        type: 'ore',
+        reqs: []
+    },
+
+    {
+        id: 'vibranium',
+        name: 'Vibranium',
+        type: 'ore',
+        reqs: []
+    },
+
+    {
+        id: 'aether',
+        name: 'Aether',
+        type: 'ore',
+        reqs: []
+    },
+
+    {
+        id: 'viterium',
+        name: 'Viterium',
+        type: 'ore',
+        reqs: []
+    },
 
     //smelts
 
@@ -175,6 +251,116 @@ var IPM_DATA = [
         reqs: [
             { id: 'bronze-bar', qty: 2 },
             { id: 'titanium', qty: 1000 }
+        ]
+    },
+
+    {
+        id: 'iridium-bar',
+        name: 'Iridium Bar',
+        type: 'smelt',
+        reqs: [
+            { id: 'steel-bar', qty: 2 },
+            { id: 'iridium', qty: 1000 }
+        ]
+    },
+
+    {
+        id: 'paladium-bar',
+        name: 'Paladium Bar',
+        type: 'smelt',
+        reqs: [
+            { id: 'platinum-bar', qty: 2 },
+            { id: 'paladium', qty: 1000 }
+        ]
+    },
+
+    {
+        id: 'osmium-bar',
+        name: 'Osmium Bar',
+        type: 'smelt',
+        reqs: [
+            { id: 'titanium-bar', qty: 2 },
+            { id: 'osmium', qty: 1000 }
+        ]
+    },
+
+    {
+        id: 'rhodium-bar',
+        name: 'Rhodium Bar',
+        type: 'smelt',
+        reqs: [
+            { id: 'iridium-bar', qty: 2 },
+            { id: 'rhodium', qty: 1000 }
+        ]
+    },
+
+    {
+        id: 'inerton-bar',
+        name: 'Inerton Bar',
+        type: 'smelt',
+        reqs: [
+            { id: 'paladium-bar', qty: 2 },
+            { id: 'inerton', qty: 1000 }
+        ]
+    },
+
+    {
+        id: 'quadium-bar',
+        name: 'Quadium Bar',
+        type: 'smelt',
+        reqs: [
+            { id: 'osmium-bar', qty: 2 },
+            { id: 'quadium', qty: 1000 }
+        ]
+    },
+
+    {
+        id: 'scrith-bar',
+        name: 'Scrith Bar',
+        type: 'smelt',
+        reqs: [
+            { id: 'rhodium-bar', qty: 2 },
+            { id: 'scrith', qty: 1000 }
+        ]
+    },
+
+    {
+        id: 'uru-bar',
+        name: 'Uru Bar',
+        type: 'smelt',
+        reqs: [
+            { id: 'inerton-bar', qty: 2 },
+            { id: 'uru', qty: 1000 }
+        ]
+    },
+
+    {
+        id: 'vibranium-bar',
+        name: 'Vibranium Bar',
+        type: 'smelt',
+        reqs: [
+            { id: 'quadium-bar', qty: 2 },
+            { id: 'vibranium', qty: 1000 }
+        ]
+    },
+
+    {
+        id: 'aether-bar',
+        name: 'Aether Bar',
+        type: 'smelt',
+        reqs: [
+            { id: 'scrith-bar', qty: 2 },
+            { id: 'aether', qty: 1000 }
+        ]
+    },
+
+    {
+        id: 'viterium-bar',
+        name: 'Viterium Bar',
+        type: 'smelt',
+        reqs: [
+            { id: 'uru-bar', qty: 2 },
+            { id: 'viterium', qty: 1000 }
         ]
     },
 
@@ -320,7 +506,171 @@ var IPM_DATA = [
             { id: 'basic-computer', qty: 5 },
             { id: 'titanium-bar', qty: 5 }
         ]
-    }
+    },
 
+    {
+        id: 'navigation-module',
+        name: 'Navigation Module',
+        type: 'craft',
+        reqs: [
+            { id: 'laser-torch', qty: 2 },
+            {id: 'thermal-scanner', qty: 1}
+        ]
+    },
+
+    {
+        id: 'plasma-torch',
+        name: 'Plasma Torch',
+        type: 'craft',
+        reqs: [
+            { id: 'iridium-bar', qty: 15 },
+            { id: 'laser-torch', qty: 5 }
+        ]
+    },
+
+    {
+        id: 'radio-tower',
+        name: 'Radio Tower',
+        type: 'craft',
+        reqs: [
+            { id: 'aluminum-bar', qty: 150 },
+            { id: 'platinum-bar', qty: 75 },
+            { id: 'titanium-bar', qty: 50 }
+        ]
+    },
+
+    {
+        id: 'telescope',
+        name: 'Telescope',
+        type: 'craft',
+        reqs: [
+            { id: 'lense', qty: 20 },
+            { id: 'advanced-computer', qty: 1 }
+        ]
+    },
+
+    {
+        id: 'satellite-dish',
+        name: 'Satellite Dish',
+        type: 'craft',
+        reqs: [
+            { id: 'steel-bar', qty: 150 },
+            { id: 'paladium-bar', qty: 30 }
+        ]
+    },
+
+    {
+        id: 'motor',
+        name: 'Motor',
+        type: 'craft',
+        reqs: [
+            { id: 'bronze-bar', qty: 500 },
+            { id: 'hammer', qty: 200 }
+        ]
+    },
+
+    {
+        id: 'accumulator',
+        name: 'Accumulator',
+        type: 'craft',
+        reqs: [
+            { id: 'osmium-bar', qty: 20 },
+            { id: 'advanced-battery', qty: 3 }
+        ]
+    },
+
+    {
+        id: 'nuclear-capsule',
+        name: 'Nuclear Capsule',
+        type: 'craft',
+        reqs: [
+            { id: 'rhodium-bar', qty: 5 },
+            { id: 'plasma-torch', qty: 1 }
+        ]
+    },
+
+    {
+        id: 'wind-turbine',
+        name: 'Wind Turbine',
+        type: 'craft',
+        reqs: [
+            { id: 'aluminum-bar', qty: 300 },
+            { id: 'motor', qty: 1 }
+        ]
+    },
+
+
+    {
+        id: 'space-probe',
+        name: 'Space Probe',
+        type: 'craft',
+        reqs: [
+            { id: 'solar-panel', qty: 25 },
+            { id: 'telescope', qty: 1 },
+            { id: 'satellite-dish', qty: 1 }
+        ]
+    },
+
+    {
+        id: 'nuclear-reactor',
+        name: 'Nuclear Reactor',
+        type: 'craft',
+        reqs: [
+            { id: 'iridium-bar', qty: 300 },
+            { id: 'nuclear-capsule', qty: 1 }
+        ]
+    },
+
+    {
+        id: 'collider',
+        name: 'Collider',
+        type: 'craft',
+        reqs: [
+            { id: 'inerton-bar', qty: 500 },
+            { id: 'quadium-bar', qty: 100 }
+        ]
+    },
+
+    {
+        id: 'gravity-chamber',
+        name: 'Gravity Chamber',
+        type: 'craft',
+        reqs: [
+            { id: 'advanced-computer', qty: 60 },
+            { id: 'nuclear-reactor', qty: 1 }
+        ]
+    },
+
+
+    {
+        id: 'robot',
+        name: 'Robot',
+        type: 'craft',
+        reqs: [
+            { id: 'scrith-bar', qty: 300 },
+            { id: 'accumulator', qty: 90 }
+        ]
+    },
+
+    {
+        id: 'fusion-capsule',
+        name: 'Fusion Capsule',
+        type: 'craft',
+        reqs: [
+            { id: 'uru-bar', qty: 200 },
+            { id: 'vibranium-bar', qty: 100 },
+            { id: 'nuclear-capsule', qty: 100 }
+        ]
+    },
+
+    {
+        id: 'teleporter',
+        name: 'Teleporter',
+        type: 'craft',
+        reqs: [
+            { id: 'navigation-module', qty: 250 },
+            { id: 'gravity-chamber', qty: 1 }
+        ]
+    },
 
 ]
